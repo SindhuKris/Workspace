c5b224e6540588bda18b2bd9a7e398ced13a34ae
Admin <admin@Admins-Air.phub.net.cable.rogers.com>
2023-04-29 00:34:46 -0400

    ng class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My_Project';
+
+
+  hideColor=true;
+
+  toggle(){
+    this.hideColor= !this.hideColor
+  }
 }
